e20f8514f566d427087525c4f0c7f15947d03fcc
Peter Walser <peter.walser@ti8m.ch>
2021-08-26 10:22:00 +0200

    Added missing Javadoc param

--- src/main/java/ch/frostnova/spring/boot/mutual/tls/api/service/WeatherService.java
+++ src/main/java/ch/frostnova/spring/boot/mutual/tls/api/service/WeatherService.java
@@ -12,6 +12,7 @@ public interface WeatherService {
     /**
      * Get forecast for the next few days
      *
+     * @param days number of days to forecast (0=today only, 1=today and tomorrow, and so on)
      * @return forecast
      */
     List<DailyWeather> getForecast(int days);
